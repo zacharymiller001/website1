0830e496482186be7dd67e2aa9bc340b3326dfdd
zacharymiller001 <142838884+zacharymiller001@users.noreply.github.com>
2024-03-01 22:47:25 +0000

    Fri, Mar 1, 2024, 2:47 PM -08:00

--- js/sounds.js
+++ js/sounds.js
@@ -4,6 +4,9 @@ sounds.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText=sound
+    btn.addEventListener('click', () => {
+        document.getElementById(sound)
+    })
 
     document.getElementById('buttons').appendChild(btn)
 })
