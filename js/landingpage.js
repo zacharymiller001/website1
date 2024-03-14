294ba45e56bdea4e05b2eb0b210fd039e342c875
zacharymiller001 <142838884+zacharymiller001@users.noreply.github.com>
2024-03-14 21:21:03 +0000

    Thu, Mar 14, 2024, 2:21 PM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,1 +1,11 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.
+})
